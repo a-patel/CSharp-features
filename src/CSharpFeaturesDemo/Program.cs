94f4f360a34ff73717db9fa7885a9285eb4f3af8
Ashish Patel <toaashishpatel@gmail.com>
2019-10-18 00:11:18 +0530

    demo project changes

--- src/CSharpFeaturesDemo/Program.cs
+++ src/CSharpFeaturesDemo/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿#region Imports
+using CSharp8Features;
+using System;
+using System.Threading.Tasks;
+#endregion
 
 namespace CSharpFeaturesDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n=== IndicesAndRanges ===");
+            IndicesAndRanges.Demo();
+
+            Console.WriteLine("\n=== SwitchExpressions ===");
+            SwitchExpressions.Demo();
+
+            Console.WriteLine("\n=== PatternMatching ===");
+            PatternMatching.Demo();
+
+            Console.WriteLine("\n=== StaticLocalFunctions ===");
+            StaticLocalFunctions.Demo();
+
+            Console.WriteLine("\n=== UsingDeclarations ===");
+            UsingDeclarations.Demo();
+
+            Console.WriteLine("\n=== AsyncStreams ===");
+            await AsyncStreams.Demo();
+
+            Console.WriteLine("\n=== TargetTypedNew ===");
+            TargetTypedNew.Demo();
+
+            Console.WriteLine("\n=== NullCoallescingAssignment ===");
+            NullCoallescingAssignment.Demo();
+
+            Console.WriteLine("\n=== DefaultInterfaceMethods ===");
+            DefaultInterfaceMethods.Demo();
+
+            Console.WriteLine("\n=== DisposableRefStruct ===");
+            DisposableRefStruct.Demo();
+
+            Console.WriteLine("\n=== UnmanagedConstructedTypes ===");
+            UnmanagedConstructedTypes.Demo();
         }
     }
 }
